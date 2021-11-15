aab8bfcc1cc7f0a5a300a6f7239adb2fa3c5287a
renniequin@gmail.com <renniequin@gmail.com>
2021-11-15 09:23:54 +0300

    fixes bugs in upvoting and downvoting

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventEmitter } from 'keyv';
 import {Quote} from '../quote'
+import{bestQuoteDirective} from '../best-quote.directive'
 
 @Component({
   selector: 'app-quote',
@@ -10,28 +11,31 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote(1, 'Ya mjinga husimamiwa na Mungu', 'God defends the simple')
+    new Quote(1, 'Ya mjinga husimamiwa na Mungu', 'God defends the simple', 'mother', 'Irene',new Date(2021,10,11))
   ]
 
+  addNewQuote(quote: Quote){
+    let quoteLength = this.quotes.length;
+    quote.id=quoteLength+1;
+    this.quotes.push(quote)
+  }
   toggleElaboration(index:number){
     this.quotes[index].showElaboration = !this.quotes[index].showElaboration;
   }
-
-  vote(isLiked: any, index: number){
-    if (isLiked){
-      for (){
-        
-      }
-      var likes= +1;
-      console.log(`This quote has ${likes} votes`)
-    }
+  public likes =0;
+  public dislikes = 0;
+  vote( isLiked: any, index: number){
+      this.likes += 1;
+      console.log(`This quote has ${this.likes} votes`)
   }
-  downVote( isDisliked: any, index: number){
+ 
+  downVote(  isDisliked: any, index: number){
     if(isDisliked){
-      var dislikes=+1;
-      console.log(`This quote has ${dislikes} dislikes`)
+      this.dislikes += 1;
+      console.log(`This quote has ${this.dislikes} dislikes`)
     }
   }
+
   deleteQuote(isDelete: any, index: number){
     if(isDelete){
       this.quotes.splice(index,1);
