0eb7cf5574eb6d5be6784f1eb23201727d5747a0
renniequin@gmail.com <renniequin@gmail.com>
2021-11-15 09:30:07 +0300

    adds date to quote constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,8 @@
+import { DatePipe } from "@angular/common";
+
 export class Quote {
     showElaboration!: boolean;
-    constructor(public id: number, public name: string, public elaboration: string){
+    constructor(public id: number, public quote: string, public elaboration: string, public author: string, public contributor: string, public date: Date ){
         this.showElaboration=false;
     }
 }
